590f18a9ee0fb55e1dc1c94d70281ada575f585c
Jefferson Dantas <jeffersonn.jjdaj@gmail.com>
2023-11-22 23:46:48 +0000

    Corrigido bugs ao concluir tarefas

--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -5,27 +5,23 @@ socketio.on('user-doing-todo', (todos) => {
   generateDashboardTable(todos)
 })
 
-socketio.on('user-done-todo', (todos) => {
-  generateDashboardTable(todos)
-})
-
 function generateDashboardTable(todos) {
   $dashboardTodos.innerHTML = ''
   for (var todo of todos) {
     var $row = document.createElement('tr')
     var $name = document.createElement('td')
-    var $users = document.createElement('td')
+    var $users_name_finished = document.createElement('td')
     var $users_finished = document.createElement('td')
     var $users_not_finished = document.createElement('td')
     var $users_name_not_finished = document.createElement('td')
     $name.innerText = todo.name
-    $users.innerText = todo.users
+    $users_name_finished.innerText = todo.users_name_finished
     $users_finished.innerText = todo.users_finished
     $users_not_finished.innerText = todo.users_not_finished
     $users_name_not_finished.innerText = todo.users_name_not_finished
 
     $row.appendChild($name)
-    $row.appendChild($users)
+    $row.appendChild($users_name_finished)
     $row.appendChild($users_finished)
     $row.appendChild($users_not_finished)
     $row.appendChild($users_name_not_finished)
